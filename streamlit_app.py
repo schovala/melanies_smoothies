f6a1d2a72a6521ace3de1aac631a741113d566c3
schovala <schovala123@gmail.com>
2024-09-23 16:09:42 -0400

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 #st.stop()
 # convert snowpark dataframe to pandas dataframe
 pd_df = my_dataframe.to_pandas()
-st.datframe(pd_df)
+st.dataframe(pd_df)
 st.stop()
 
 ingrediants_list = st.multiselect(
