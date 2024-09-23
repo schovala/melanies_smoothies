60b01acdd6b55c26c5c9efa6c1a34cc33bbcdd45
schovala <schovala123@gmail.com>
2024-09-23 15:35:24 -0400

    Update streamlit_app.py
    moved fruityvice to fruit choose option

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -30,6 +31,8 @@ if ingrediants_list:
 
     for fruit_chosen in ingrediants_list:
         ingrediants_string += fruit_chosen + ' '
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe (data = fruityvice_response.json(),use_container_width = True)
 
     #st.write(ingrediants_string)
 
@@ -44,9 +47,3 @@ if ingrediants_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered! {name_on_order}', icon="✅")
-
-# to include fruityvice neutrition information
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df = st.dataframe (data = fruityvice_response.json(),use_container_width = True)
